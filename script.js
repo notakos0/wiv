54fe1c376e15ee6af766623aa08d30511121f2dd
NotAkos <109524305+notakos0@users.noreply.github.com>
2025-02-21 16:33:26 +0100

    na animaciok

--- script.js
+++ script.js
@@ -27,3 +27,27 @@ function toWsup() {
 function toggleMenu() {
     document.querySelector(".navbcon").classList.toggle("active");
 }
+
+//loading
+
+window.addEventListener("load", () => {
+    const fadeElement = document.querySelector(".fade");
+    setTimeout(() => {
+        fadeElement.classList.add("hidden");
+    }, 1000);
+});
+
+const slideloadElements = document.querySelectorAll(".slideload");
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("active");
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+slideloadElements.forEach(el => observer.observe(el));
